68ec7d185f3a7f1a450eea5b1d0f8e23336ec638
YuvalSuede <yuvalsuede@gmail.com>
2019-06-23 13:55:00 +0300

    support https

--- src/app/config/app.config.ts
+++ src/app/config/app.config.ts
@@ -7,8 +7,8 @@ export interface AppConfig {
 }
 
 export const OMDB_CONFIG: AppConfig = {
-    apiEndpoint: 'http://www.omdbapi.com/?apikey=5f6a5ccd&',
-    posterEndpoint: 'http://img.omdbapi.com/?apikey=5f6a5ccd&',
+    apiEndpoint: 'https://www.omdbapi.com/?apikey=5f6a5ccd&',
+    posterEndpoint: 'https://img.omdbapi.com/?apikey=5f6a5ccd&',
 };
 
 export let APP_CONFIG = new InjectionToken<AppConfig>('app.config');
